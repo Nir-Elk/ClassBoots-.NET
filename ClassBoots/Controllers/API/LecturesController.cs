725e4f477c792c711e01e6e61e739153ad2bda9c
iluzamit <34029258+iluzamit@users.noreply.github.com>
2018-08-28 20:50:35 +0300

    Restful API.

---
+++ ClassBoots/Controllers/API/LecturesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ClassBoots.Data;
+using ClassBoots.Models;
+
+namespace ClassBoots.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LecturesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LecturesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Lecture>> GetAll()
+        {
+            return _context.Lecture.ToList();
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<Lecture> GetById(int id)
+        {
+            var item = _context.Lecture.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+    }
+}
--- ClassBoots/Data/Migrations/20180828161704_iluz.Designer.cs
+++ ClassBoots/Data/Migrations/20180828161704_iluz.Designer.cs
@@ -4,14 +4,16 @@ using ClassBoots.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ClassBoots.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180828161704_iluz")]
+    partial class iluz
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
