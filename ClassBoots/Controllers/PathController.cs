7e27e445a1a8a286db1778c03e361cfab9e76164
Aviv <avivsegal@gmail.com>
2018-09-08 15:28:38 +0300

    Some work on menu

---
+++ ClassBoots/Controllers/PathController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ClassBoots.Models;
+
+
+namespace ClassBoots.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PathController : ControllerBase
+    {
+        private readonly ModelContext _context;
+
+        public PathController(ModelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Path
+        [HttpGet]
+        public IEnumerable<Institution> GetInstitutions()
+        {
+            return _context.Institution;
+        }
+
+        [HttpGet("{institution_id}")]
+        public IEnumerable<School> GetSchools([FromRoute] int institution_id)
+        {
+            return _context.School.Where(s => s.InstitutionID == institution_id).ToList();
+        }
+
+
+
+
+    }
+}
--- ClassBoots/wwwroot/js/navigator.js
+++ ClassBoots/wwwroot/js/navigator.js
@@ -1,141 +1,68 @@
+/* avivsegal@gmail.com */
 $(function () {
-    var path = [];
-    switch (path.length) {
+    var area = $('#sidebarnav');
+    var emptyMenuItem = $('#menuitem');
+    var emptyPreviousBtn = $('#previousbtn');
+    emptyMenuItem.hide();
+    emptyPreviousBtn.hide();
 
-        // api/{institution_id}/schools
-        case 1:
- 
-            break;
-
-        // api/{institution_id}/{school_id}/subjects
-        case 2:
-            
-            break;
-
-        // api/{institution_id}/{school_id}/{subject_id}/lectures
-        case 2:
-            
-            break;
-
-        // api/{institution_id}/{school_id}/{subject_id}/{lecture_id}/videos
-        case 3:
-            
-            break;
-        //.....
-
-
-         // api/institutions
-        default:
-            $.get('/api/institutions', function (institutions) {
-                $.each(institutions, function () {
-                    let newMenuItem = $('#menuitem').clone();
-                    let path = '/' + this.id + '/schools';
-                    newMenuItem.attr('id', 'institution_' + this.id);
-                    newMenuItem.find('a').attr('href', path);
-                    newMenuItem.find('a').find('span').html(this.name);
-                    newMenuItem.find('a').click(function (event) {
-                        console.log(path);
-                        history.pushState(this.name , null, path);
-                        return event.preventDefault();
-                    });
-                    $('#sidebarnav').append(newMenuItem);
-                });
-            });
+    //$.session.clear();  /* Uncommet to destroy session. */
+    if (!$.session.get('path')) {
+        $.session.set('path', '/api/path')
     }
 
-    // Revert to a previously saved state
-    window.addEventListener('popstate', function (event) {
-        console.log('popstate fired!');
-        //updateContent(event.state);
-    });
+    var menuClickHandler = function (nextPath) {
+        $('#path').html(nextPath);
+        $.session.set('path', nextPath);
+        renderMenu();
+    }
+
+    var generatePrevBtn = function () {
+        let prev = $.session.get('path');
+        prev = prev.split('/');
+        prev.pop();
+        prev = prev.join('/');
+        let newPrevBtn = emptyPreviousBtn.clone();
+
+        newPrevBtn.find('a').attr('href', prev);
+        newPrevBtn.removeAttr('id');
+        newPrevBtn.click(function (e) {
+            e.preventDefault();
+            menuClickHandler(prev);
+        });
+        newPrevBtn.show();
+        return newPrevBtn;
+    }
+
+    var renderMenu = function () {
+        let url = $.session.get('path');
+        area.html('');
+        if (url != '/api/path') {
+            area.append(generatePrevBtn());
+        }
+        $.get(url, function (data) {
+            if (data.length == 0) {
+                area.append("No items here.<br>Be the first to add items :)")
+            } else {
+                $.each(data, function () {
+                    let newMenuItem = emptyMenuItem.clone();
+                    let nextPath = url + '/' + this.id;
+                    newMenuItem.find('a').attr('href', nextPath);
+                    newMenuItem.find('a').append(this.name);
+                    newMenuItem.click(function (e) {
+                        e.preventDefault();
+                        menuClickHandler(nextPath);
+                    });
+                    newMenuItem.show();
+                    area.append(newMenuItem);
+                });
+            }
+        }).fail(function (e) {
+            area.append('There is an error ): <br>'
+                + JSON.stringify(JSON.parse(JSON.stringify(e)), null, 2)
+                + '<br>Please Context for any help:<br>' + '<b>avivsegal@gmail.com</b>');
+        });
+    }
+
+    renderMenu();
 });
-
-
-//$('#default-button').hide();
-//var sideNav = $('#sidebarnav');
-
-//var current = '';
-
-//var changeToInst = function (parent) {
-//        $('#abovesidebar').html("");
-//        current = '';
-//        clear();
-//        $.get('/api/Institutions', function (data) {
-//            renderItems(data, changeToSchools);
-//        });
-//}
-
-//var changeToSchools = function (parentId) {
-//    if (current != 'schools') {
-
-//        current = 'schools';
-//        clear();
-//        $.get('/api/Institutions/' + parentId + '/Schools', function (data) {
-//            renderItems(data, changeToSubjects);
-//            $('#abovesidebar').html(BackToBtn(() => changeToInst(null)));
-//        });
-//    }
-//}
-
-//var changeToSubjects = function (parentId) {
-//    if (current != 'subjects') {
-//        current = 'subjects';
-//        clear();
-//        $.get('/api/Schools/' + parentId + '/Subjects', function (data) {
-//            renderItems(data, changeToLectures);
-//            $('#abovesidebar').html(BackToBtn(() => changeToSchools(parentId)));
-//        });
-//    }
-//}
-
-//var changeToLectures = function (parentId) {
-//    if (current != 'lectures') {
-//        current = 'lectures';
-//        clear();
-//        $.get('/api/Lectures/' + parentId + '/Videos', function (data) {
-//            renderItems(data, null);
-//            $('#abovesidebar').html(BackToBtn(() => changeToSubjects(parentId)));
-//        });
-//    }
-//}
-
-//var appendRow = function (title, image) {
-//    var $row = $('#default-button').clone();
-//    $row.removeAttr('id');
-//    $row.find('.btn-ref').find('.btn-img').attr('src', image)
-//    $row.find('.btn-ref').append(title);
-//    return $row;
-//}
-
-//var BackToBtn = function (callback) {
-//    var $btn = $('#backtobutton').clone();
-
-//    $btn.click(function () {
-//        callback();
-//    });
-
-//    $btn.show();
-//    return $btn;
-//}
-//var clear = function () {
-//    sideNav.html("");
-//}
-//var renderItems = function (data, changeTo) {
-//    $.each(data, function () {
-//        var $row = appendRow(this.name, this.image);
-//        var ParentId = this.id;
-//        var ParentRow = $row;
-//        $row.click(function (e) {
-//            e.preventDefault();
-//            e.stopPropagation();
-//            changeTo(ParentId);
-//        });
-//        $row.show();
-//        sideNav.append($row);
-//    });
-
-//}
-
-//$(function() {
-//    changeToInst();
-//});
