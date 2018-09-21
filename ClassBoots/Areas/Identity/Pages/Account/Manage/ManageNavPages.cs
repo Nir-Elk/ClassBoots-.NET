e86ac7ab2d77e175f07886b9030758045ea37e75
Nir-Elk <nirelkayam31@gmail.com>
2018-09-21 16:58:40 +0300

    panel admin (not ready)

--- ClassBoots/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ ClassBoots/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -9,6 +9,7 @@ namespace ClassBoots.Areas.Identity.Pages.Account.Manage
     public static class ManageNavPages
     {
         public static string Index => "Index";
+        public static string Admin => "Administrator";
 
         public static string ChangePassword => "ChangePassword";
 
@@ -20,9 +21,8 @@ namespace ClassBoots.Areas.Identity.Pages.Account.Manage
 
         public static string PersonalData => "PersonalData";
 
-        public static string TwoFactorAuthentication => "TwoFactorAuthentication";
-
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+        public static string AdminNavClass(ViewContext viewContext) => PageNavClass(viewContext, Admin);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
 
@@ -33,9 +33,7 @@ namespace ClassBoots.Areas.Identity.Pages.Account.Manage
         public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
 
         public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
-
-        public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
-    
+   
         public static string PageNavClass(ViewContext viewContext, string page)
         {
             var activePage = viewContext.ViewData["ActivePage"] as string
